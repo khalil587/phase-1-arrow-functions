b3c77f31497dfb7f78cc12487bdcc81afbfbe1fa
Khalil Lindsay <lindsay.khalil@gmail.com>
2021-06-28 14:33:05 -0400

    commit tree 70132f31cc60f20d43b098b9dd9f8495baed643b

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = function (){
+    return(2000 / 100);
+}
+const square = (num1=2, num2=2) => num1**num2;{
+}
+num5=3;
+num4=5;
+function add(num5, num4){
+    return(num5 + num4);
+}
+    
